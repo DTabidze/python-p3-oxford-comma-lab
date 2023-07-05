e8cc4cf5a4da9d04c97acfaddac6014c2b7e4c0e
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-05 13:41:21 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    if len (items) == 2:
+        return f"{items[0]} and {items[1]}"
+    str = ', '.join(items[:-1]) + f", and {items[-1]}"
+    # left += f", and {items[-1]}"
+    return str
+
+print (oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
